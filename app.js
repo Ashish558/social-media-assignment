1a8d6e15dd3ec4902138b025d6b54c14b2448827
Ashish Prasad <77892788+Ashish558@users.noreply.github.com>
2022-03-17 13:50:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,5 +35,11 @@ app.use(routes)
 
 const PORT = process.env.PORT || 4000
 
+app.use(express.static("client/build"))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
+
 app.listen(PORT)
 console.log('server on')
