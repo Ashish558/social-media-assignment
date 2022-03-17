65739e10ef456c7e39b21f045f1a14f40885d6ab
Ashish Prasad <77892788+Ashish558@users.noreply.github.com>
2022-03-17 13:52:28 +0530

    Update constants.js

--- client/src/services/constants.js
+++ client/src/services/constants.js
@@ -1,7 +1,5 @@
 
-// export const domain = 'https://desolate-mesa-23396.herokuapp.com'
-export const domain = 'http://localhost:4000'
-
+export const domain = 'https://social-media-234.herokuapp.com'
 
 export const authHeader = {
     headers: {
